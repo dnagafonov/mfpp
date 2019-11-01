84dc155c2a7ba21351da443c347d1129c1cf2ae6
dnagafonov <denisagafonovwork@gmail.com>
2019-11-01 21:05:07 +0300

    fix unworked outline: none

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Header from "./components/header";
+import Header from "../header";
+import './app.css'
 
 function App() {
   return (
-    <div>
+    <div className='general-options'>
       <Header/>
     </div>
   );
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,16 +8,18 @@ export default class Header extends React.Component {
         return(
             <header className="header">
                 <div className='header-logo'>
-                    <div className='header-title-text'>brand</div>
+                    <div className='header-title-text'>
+                        brand
+                    </div>
                 </div>
                 <div className='header-buttons-group'>
-                    <button className='header-button'>
+                    <button className='custom-button'>
                         Info
                     </button>
-                    <button className='header-button'>
+                    <button className='custom-button'>
                         Album
                     </button>
-                    <button className='header-button'>
+                    <button className='custom-button'>
                         Contacts
                     </button>
                 </div>
